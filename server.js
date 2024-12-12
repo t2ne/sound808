74e4c4a9d4f591203cd0f4f1a9b692287d2054e6
António Rebelo <imtoninho@gmail.com>
2024-12-12 14:07:43 +0000

    Second

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const corsOptions = {
-  origin: "http://localhost",
+  origin: ["http://localhost", "http://localhost:3000"],
 };
 
 app.use(cors(corsOptions));
