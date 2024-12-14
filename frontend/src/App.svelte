80e03a0544737e6d3e11e967067172916e348f6b
António Rebelo <imtoninho@gmail.com>
2024-12-14 21:38:25 +0000

    serve pra frontend tmb

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,30 +1,47 @@
 <script>
-	export let name;
+  export let name;
+  let movies = [];
+
+  fetch("http://localhost:3000/api/movies") // Update this URL if needed
+    .then((res) => res.json())
+    .then((data) => {
+      movies = data;
+    });
 </script>
 
+<h1>Movies</h1>
+<ul>
+  {#each movies as movie}
+    <li>{movie.title}</li>
+  {/each}
+</ul>
+
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h1>Hello {name}!</h1>
+  <p>
+    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+    how to build Svelte apps.
+  </p>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
